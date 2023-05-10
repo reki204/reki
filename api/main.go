7dbd5821b882c07009239844aeb421ab78a95901
Onda Reki <rekionda0524@ezweb.ne.jp>
2023-05-10 17:08:07 +0900

    golang: エラーハンドリング設定

--- api/main.go
+++ api/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/reki204/reki/server"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "welcome",
-		})
-	})
-	r.Run()
+	server.Init()
 }
--- api/server/server.go
+++ api/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"log"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -9,13 +10,16 @@ import (
 
 func Init() {
 	r := router()
-	r.Run()
+	if err := r.Run(); err != nil {
+		log.Fatalf(err.Error())
+	}
 }
 
 func router() *gin.Engine {
 	r := gin.Default()
+	r.HandleMethodNotAllowed = true
 
-	setCORS(r)
+	r.Use(setCORS(r))
 
 	// ルーティング
 	r.GET("/", func(c *gin.Context) {
@@ -24,29 +28,33 @@ func router() *gin.Engine {
 		})
 	})
 
+	// 404エラー
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{
+			"message": "Not Found",
+		})
+	})
+
+	// 500エラー
+	r.NoMethod(func(c *gin.Context) {
+		c.JSON(500, gin.H{
+				"message": "Internal Server Error",
+		})
+	})
+
 	return r
 }
 
 // CORS
-func setCORS(r *gin.Engine) {
-	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{
-			"http://localhost:3000",
-		},
-		AllowMethods: []string{
-			"POST",
-			"GET",
-			"OPTIONS",
-		},
-		AllowHeaders: []string{
-			"Access-Control-Allow-Credentials",
-			"Access-Control-Allow-Headers",
-			"Content-Type",
-			"Content-Length",
-			"Accept-Encoding",
-			"Authorization",
-		},
-		AllowCredentials: true,
-		MaxAge: 24 * time.Hour,
-	}))
+func setCORS(r *gin.Engine) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		r.Use(cors.New(cors.Config{
+			AllowOrigins: []string{"http://localhost:3000",},
+			AllowMethods: []string{"POST","GET","OPTIONS",},
+			AllowHeaders: []string{"Content-Type",},
+			AllowCredentials: true,
+			MaxAge: 24 * time.Hour,
+		}))
+		c.Next()
+	}
 }
