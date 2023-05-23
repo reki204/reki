40421e2f6fb16a62aa27f13a410c891b0a9d000a
Onda Reki <rekionda0524@ezweb.ne.jp>
2023-05-23 23:13:25 +0900

    golang: main.go修正

--- api/main.go
+++ api/main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"context"
+	"log"
+	"os"
 
+	"github.com/reki204/reki/config"
 	"github.com/reki204/reki/server"
 )
 
 func main() {
-	server.Init(context.Background())
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminated server: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		return err
+	}
+
+	// サーバー起動
+	log.Printf("Listening and serving HTTP on :%v", cfg.Port)
+	listenServer := server.Init(ctx)
+	return listenServer
 }
