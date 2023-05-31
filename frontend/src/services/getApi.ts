0cbef6098156671711c37ee79551061b1f513528
Onda Reki <rekionda0524@ezweb.ne.jp>
2023-05-31 18:37:25 +0900

    frontend: apiのデータをfetch

--- frontend/src/services/getApi.ts
+++ frontend/src/services/getApi.ts
@@ -1,21 +1,15 @@
 import axios from "axios";
 import { MessageType } from "@/types";
 
-const url = "http://127.0.0.1:8080/api/v1/";
+const API_URL = "http://localhost:8080/api/v1/";
 
 export const getMessage = async (): Promise<MessageType> => {
-  // const result = await axios.get(url).then((response) => response.data);
-  // // return result;
-  // console.log("res", result.data);
-
-  // return result.data;
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(API_URL);
     const result = response.data;
-    console.log("res", result);
+    console.log(result);
     return result;
   } catch (error) {
-    // エラーハンドリング
     console.error("エラーが発生しました:", error);
     throw error;
   }
